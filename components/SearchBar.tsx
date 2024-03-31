e624eb8b259ae2e9377b9d36a92c90b11635793b
DaysonTai1848 <daysontai69@gmail.com>
2024-03-31 18:55:36 +0800

    Make search bar better

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,9 +3,12 @@ import { CiSearch } from "react-icons/ci";
 
 const SearchBar = () => {
   return (
-    <form className="flex flex-row border py-2 px-4 justify-center items-center border-black rounded-full">
+    <form className="flex flex-row border py-2 px-4 justify-center items-center border-black rounded-full space-x-3">
       <CiSearch className="hover:" />
-      <input className="border-none"></input>
+      <input
+        className="focus:outline-none text-sm font-light text-slate-900"
+        placeholder="Search for anything"
+      />
     </form>
   );
 };
